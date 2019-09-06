19077f50b0dd4b16873ee8e241abe8ba0579386e
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-07 01:18:34 +0300

    Second component

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="employee-form">
+    <form method="POST">
+      <label>Employee name</label>
+      <input type="text" />
+      <label>Employee Email</label>
+      <input type="text" />
+      <button>Add Employee</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "employee-form"
+};
+</script>
+
+<style scoped>
+</style>
