558c65b471284a4ad4e4de91a3938ac5354ab952
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-07 00:55:55 +0300

    added data method

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <template>
-  <div id="app"></div>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+
+    <employee-table />
+  </div>
 </template>
 
 <script>
+import EmployeeTable from "@/components/EmployeeTable";
+
 export default {
   name: "app",
-  components: {}
+  components: {
+    EmployeeTable
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Richard Hendricks",
+          email: "richard@piedpiper.com"
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com"
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com"
+        }
+      ]
+    };
+  }
 };
 </script>
 
