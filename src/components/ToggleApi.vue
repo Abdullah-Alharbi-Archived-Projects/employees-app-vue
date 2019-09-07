551acd77710d1d4823670cdfde884d6e6e3457cf
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-07 17:35:52 +0300

    fixed errors

---
+++ src/components/ToggleApi.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="toggle-api" class="float-right">
+    <button
+      :class="!callAPI ? 'red' : 'accent-button'"
+      @click.prevent="handleToggle"
+    >Call API [{{ callAPI ? "active" : "not active" }}]</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "toggle-api",
+  props: ["callAPI"],
+  methods: {
+    handleToggle() {
+      this.$emit("call:api");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.red {
+  background-color: #ee0317;
+  border-color: #cc0314;
+}
+</style>
