b4c4bf2d2ed5742a72d337667eb9e30480dd5ae1
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-07 17:23:16 +0300

    toggle api button

---
+++ src/components/ToggleApiCalls.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="toggle-api">
+    <button
+      @click.prevent="handleToggle"
+      :class="{ 'disabled': callAPI }"
+    >Call API [{{ callAPI ? "active" : "not active" }}]</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "toggle-api",
+  props: ["callAPI"],
+  methods: {
+    handleToggle() {
+      this.$emit("call:api");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.disabled {
+  background-color: #cdcdcd;
+}
+</style>
