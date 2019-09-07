992387471434de7b51700a5c88afbf59cc7cff56
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-07 16:01:36 +0300

    Third component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="search-employee">
+    <form method="GET">
+      <input type="text" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "search-input"
+};
+</script>
+
+<style scoped>
+</style>
