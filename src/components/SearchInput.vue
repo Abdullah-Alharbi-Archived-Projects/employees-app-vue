36705f99a07bf9c9bcd4185d572b91c6d7d3bec4
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-07 16:05:02 +0300

    added search by

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,14 +1,30 @@
 <template>
   <div id="search-employee">
-    <form method="GET">
-      <input type="text" />
+    <form method="GET" @submit.prevent>
+      <input type="text" v-model="search" />
+      <select v-model="searchType">
+        <option value>Search by...</option>
+        <option value="name">Name</option>
+        <option value="email">Email</option>
+      </select>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "search-input"
+  name: "search-input",
+  data() {
+    return {
+      searchType: "",
+      search: ""
+    };
+  },
+  methods: {
+    handleSearch() {
+      // TODO: Emitt event search:employee
+    }
+  }
 };
 </script>
 
